0e8fbefea7115137eab326b1f43e6710a5352b68
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-12-22 15:50:11 -0500

    Fix hittable_list hit detection logic

--- src/hittable_list.cpp
+++ src/hittable_list.cpp
@@ -20,13 +20,13 @@ bool hittable_list::hit(const ray& r, double ray_tmin, double ray_tmax, hit_reco
     double closest_so_far = ray_tmax;
 
     for(const auto& object : objects){
-        if(object->hit(r, ray_tmin, closest_so_far, temp_rec)){
-            hit_anything = true;
-            closest_so_far = temp_rec.getT();
-            rec = temp_rec;
+        if(!object->hit(r, ray_tmin, closest_so_far, temp_rec)){
+            continue;
         }
 
-        hit_anything = false;
+        hit_anything = true;
+        closest_so_far = temp_rec.getT();
+        rec = temp_rec;
     }
 
     return hit_anything;
