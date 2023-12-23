447f6d13c461e03aa255711e10f62d86f7e231d7
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-12-22 20:44:54 -0500

    Remove include statements from constants header

--- src/constants.h
+++ src/constants.h
@@ -3,12 +3,9 @@
 
 #include <cmath>
 #include <limits>
-#include <memory>
-#include "ray.h"
-#include "vec3.h"
 
-const double infinity = std::numeric_limits<double>::infinity();
-const double pi = M_PI;
+constexpr double infinity = std::numeric_limits<double>::infinity();
+constexpr double pi = M_PI;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
