83653ee9b98ee583085cffb54d408abfbfe99e98
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-12-22 20:51:54 -0500

    Refactor use to interval class

--- src/sphere.h
+++ src/sphere.h
@@ -4,11 +4,12 @@
 #include "hittable.h"
 #include "hit_record.h"
 #include "vec3.h"
+#include "interval.h"
 
 class sphere : public hittable {
   public:
     sphere(point3 _center, double _radius);
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override;
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override;
   private:
     point3 center;
     double radius;
