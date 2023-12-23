9d88a3eac9dcb120256191ecf9824fe146f3dfe2
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-12-22 20:45:35 -0500

    Refactor hit method declaration to use interval class

--- src/hittable.h
+++ src/hittable.h
@@ -3,11 +3,12 @@
 
 #include "ray.h"
 #include "hit_record.h"
+#include "interval.h"
 
 class hittable {
   public:
     virtual ~hittable() = default;
-    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+    virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
 };
 
 #endif
