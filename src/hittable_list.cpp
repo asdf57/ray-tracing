203741cf2554b3d36c6b86041e21b075bc3dcc3f
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-12-22 20:49:37 -0500

    Refactor hit method to use interval class

--- src/hittable_list.cpp
+++ src/hittable_list.cpp
@@ -14,18 +14,19 @@ void hittable_list::add(std::shared_ptr<hittable> object){
     objects.push_back(object);
 }
 
-bool hittable_list::hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const{
+bool hittable_list::hit(const ray& r, interval ray_t, hit_record& rec) const{
     hit_record temp_rec;
+    double closest_so_far;
     bool hit_anything = false;
-    double closest_so_far = ray_tmax;
 
     for(const auto& object : objects){
-        if(!object->hit(r, ray_tmin, closest_so_far, temp_rec)){
+        if(!object->hit(r, ray_t, temp_rec)){
             continue;
         }
 
         hit_anything = true;
         closest_so_far = temp_rec.getT();
+        ray_t.setMax(closest_so_far);
         rec = temp_rec;
     }
 
